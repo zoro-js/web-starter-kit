11bd3ab65b58e490e25dc0dce7c56935aae7d79c
ZhangYingya <zyy@ZhangYingyas-MacBook-Pro.local>
2016-06-25 21:53:52 +0800

    add folders

---
+++ src/js/entry/test.js
@@ -0,0 +1,9 @@
+/**
+* @Author: Yingya Zhang <zyy>
+* @Date:   2016-06-25 21:42:00
+* @Email:  zyy7259@gmail.com
+* @Last modified by:   zyy
+* @Last modified time: 2016-06-25 21:43:87
+*/
+
+module.exports = {}

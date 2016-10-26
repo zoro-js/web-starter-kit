55430e49be3a483077f4c853d5440bc45ba862a1
Yinyga Zhang <zyy7259@gmail.com>
2016-10-26 11:04:28 +0800

    add yarn

--- dist/js/folder/index.js
+++ dist/js/folder/index.js
@@ -1,2 +1,14 @@
-webpackJsonp([2,0],[function(o,c){"use strict";console.log("foo")}]);
-//# sourceMappingURL=index.js.map
+webpackJsonp([0,2],[
+/* 0 */
+/*!**************************************!*\
+  !*** ./src/js/entry/folder/index.js ***!
+  \**************************************/
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	console.log('foo');
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvanMvZW50cnkvZm9sZGVyL2luZGV4LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFBQSxTQUFRQyxHQUFSLENBQVksS0FBWixFIiwiZmlsZSI6ImZvbGRlci9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdmb28nKVxuXG5cblxuLyoqIFdFQlBBQ0sgRk9PVEVSICoqXG4gKiogLi9zcmMvanMvZW50cnkvZm9sZGVyL2luZGV4LmpzXG4gKiovIl0sInNvdXJjZVJvb3QiOiIifQ==
